4d1c0d8a1ef67ce393e4f35a44f830fd7cfc0504
Joel <joelboh@gmail.com>
2009-06-16 03:43:11 +0200

    Frontend: Refactored the map component.

--- frontend/scripts/map.js
+++ frontend/scripts/map.js
@@ -1,185 +1,120 @@
-function DragTileMap(target) {
+Map = ( function() {
 
-    this.tilesize = 256;
+    var MapCreator = function() {};
 
-    // general properties
-    this.target = $(target);
-    this.shownMap = [];
+    MapCreator.prototype.init = function(target, x, y) {
+        this.map = $(target);
 
-    // dom properties / handlers
-    this.vp = $(document.createElement('div'));
-    this.vp.attr('id', 'scrollMapViewport');
-    this.vp.css({'position': 'relative', 'overflow': 'hidden', 'height': ($(window).height() - parseInt(this.target.offset().top, 10))+'px'});
-    this.target.append(this.vp);
+        // Create viewport
+        this.viewport = $(document.createElement('div')).attr('id', 'map-viewport')
+            .css({'position': 'relative', 'overflow': 'hidden', 'height': ($(window).height() - parseInt(this.map.offset().top, 10))+'px'});
+        this.map.append(this.viewport);
 
-    this.mapc = $(document.createElement('div'));
-    this.mapc.attr('id', 'scrollMapCanvasContainer');
-    this.mapc.css({'position': 'absolute', 'top': '0px', 'left': '0px'});
-    this.vp.append(this.mapc);
+        // Create scroll
+        this.scroll = $(document.createElement('div')).attr('id', 'map-scroll')
+            .css({'position': 'absolute', 'top': '0px', 'left': '0px'});
+        this.viewport.append(this.scroll);
 
-    this.canvas = $(document.createElement('div'));
-    this.canvas.attr('id', 'MapCanvas');
-    this.canvas.css({'position': 'absolute', 'top': '0px', 'left': '0px', 'zIndex': '5'});
-    this.mapc.append(this.canvas);
+        // Create canvas
+        this.canvas = $(document.createElement('div')).attr('id', 'map-canvas')
+            .css({'position': 'absolute', 'top': '0px', 'left': '0px', 'zIndex': '5'});
+        this.scroll.append(this.canvas);
 
-    this.map = $(document.createElement('div'));
-    this.map.attr('id', 'scrollMapCanvas');
-    this.map.css({'position': 'absolute', 'top': '0px', 'left': '0px'});
-    this.mapc.append(this.map);
-
-    // handle events
-    this.vp.bind("mousedown", this, function(e) {
-
-        e.data.target_offset = e.data.target.offset();
-        var page = [e.pageX - e.data.target_offset.left, e.pageY - e.data.target_offset.top];
-
-        var xpos = page[0] - parseInt(e.data.mapc.css('left'), 10);
-        var ypos = (e.data.tilesize) + (parseInt(e.data.mapc.css('top'), 10) - page[1]);
-
-        $('#debug-1').text('Position: ('+ xpos + ', ' + ypos + ')');
-
-        var temp = new Array();
-        temp = e.target.id.split('_');
-        if(temp.length == 3) {
-            var xbounds = [(parseInt(temp[1], 10) * e.data.tilesize), (parseInt(temp[1], 10) * e.data.tilesize + (e.data.tilesize-1))];
-            var ybounds = [(parseInt(temp[2], 10) * e.data.tilesize), (parseInt(temp[2], 10) * e.data.tilesize + (e.data.tilesize-1))];
-            $('#debug-2').text('Tile X: ' + temp[1] + ' [' + xbounds[0] + ', ' + xbounds[1] + ']');
-            $('#debug-3').text('Tile Y: ' + temp[2] + ' [' + ybounds[0] + ', ' + ybounds[1] + ']');
-        } else {
-            $('#debug-2').text('No X tile');
-            $('#debug-3').text('No Y tile');
+        // If possible at this stage, center map on x,y coordinates
+        if(x !== undefined && y !== undefined) {
+            this.scroll.css('top', (((this.map.height()/2) + y))+'px');
+            this.scroll.css('left', ((this.map.width()/2) + -x)+'px');
         }
 
-        if(!e.data.mapc.moving) {
-            e.data.mapc.posx = page[0];
-            e.data.mapc.posy = page[1];
-            e.data.mapc.moving = true;
-            e.data.target.everyTime(400, function(i) {
-                e.data.redraw();
-            }, 25);
+        // Setup event handlers
+        $(this.viewport).bind("mousedown", this, this.down);
+        $(document).bind("mousemove", this, this.move);
+        $(document).bind("mouseup", this, this.up);
+        $(window).bind("resize", this, this.resize);
+    };
+
+    MapCreator.prototype.down = function(e) {
+        e.data.mapOffset = e.data.map.offset();
+        var page = [e.pageX - e.data.mapOffset.left, e.pageY - e.data.mapOffset.top];
+
+        var xpos = page[0] - parseInt(e.data.scroll.css('left'), 10);
+        var ypos = parseInt(e.data.scroll.css('top'), 10) - page[1];
+
+        console.log(xpos, ypos);
+
+        if(!e.data.moving) {
+            e.data.posx = page[0];
+            e.data.posy = page[1];
+            e.data.moving = true;
         }
         return false;
-    });
-        
-    $(document).bind("mousemove", this, function(e) {
-        if(e.data.mapc.moving) {
-            var cposx = e.pageX - e.data.target_offset.left;
-            var cposy = e.pageY - e.data.target_offset.top;
-            e.data.mapc.css('top', (parseInt(e.data.mapc.css('top'), 10) + (cposy - e.data.mapc.posy))+'px');
-            e.data.mapc.css('left', (parseInt(e.data.mapc.css('left'), 10) + (cposx - e.data.mapc.posx))+'px');
-            e.data.mapc.posx = cposx;
-            e.data.mapc.posy = cposy;
+    };
+
+    MapCreator.prototype.move = function(e) {
+        if(e.data.moving) {
+            var cposx = e.pageX - e.data.mapOffset.left;
+            var cposy = e.pageY - e.data.mapOffset.top;
+            e.data.scroll.css('top', (parseInt(e.data.scroll.css('top'), 10) + (cposy - e.data.posy))+'px');
+            e.data.scroll.css('left', (parseInt(e.data.scroll.css('left'), 10) + (cposx - e.data.posx))+'px');
+            e.data.posx = cposx;
+            e.data.posy = cposy;
         }
         return false;
-    });
 
-    $(document).bind("mouseup", this, function(e) {
-        if(e.data.mapc.moving) {
-            e.data.target.stopTime();
-            e.data.mapc.moving = false;
-            e.data.redraw();
+    };
+
+    MapCreator.prototype.up = function(e) {
+        if(e.data.moving) {
+            e.data.moving = false;
         }
         return false;
-    });
+    };
 
-    $(window).bind("resize", this, function(e) {
-        // Set the height of the viewport
-        e.data.vp.css('height', ($(window).height() - parseInt(e.data.target.offset().top, 10))+'px');
+    MapCreator.prototype.resize = function(e) {
+        e.data.viewport.css('height', ($(window).height() - parseInt(e.data.map.offset().top, 10))+'px');
+    };
 
-        e.data.oldvpCols = e.data.vpCols;
-        e.data.oldvpRows = e.data.vpRows;
+    MapCreator.prototype.addObjects = function(objects) {
+        this.objects = objects;
+    };
 
-        // Number of columns and rows in the viewport
-        e.data.vpCols = Math.round((e.data.target.width() / e.data.tilesize) + .5)+1;
-        e.data.vpRows = Math.round((e.data.target.height() / e.data.tilesize) + .5)+1;
-
-        // First draw of the map
-        e.data.redraw();
-    });
-
-    // methods
-    this.init = function(x, y) {
-        // Set center values so we always can get them fast
-        this.center = [x, y];
-
-        // Number of columns and rows in the viewport
-        this.vpCols = this.oldvpCols = Math.round((this.target.width() / this.tilesize) + .5)+1;
-        this.vpRows = this.oldvpRows = Math.round((this.target.height() / this.tilesize) + .5)+1;
-
-        // Center map on x,y
-        this.mapc.css('top', (((this.target.height()/2) + y)-(this.tilesize))+'px');
-        this.mapc.css('left', ((this.target.width()/2) + -x)+'px');
-
-        // Calculate most left column and most top row
-        this.oldStartRow = Math.round((parseInt(this.mapc.css('top'), 10)/this.tilesize)+.5) +1;
-        this.oldStartCol = -1*Math.round((parseInt(this.mapc.css('left'), 10)/this.tilesize)+.5) -1;
-
-        // First draw of the map
-        this.redraw();
-    }
-    this.redraw = function() {
-
-        // Calculate most left column and most top row (like in init)
-        var startRow = Math.round((parseInt(this.mapc.css('top'), 10)/this.tilesize)+.5) +1;
-        var startCol = -1*Math.round((parseInt(this.mapc.css('left'), 10)/this.tilesize)+.5) -1;
-
-        // Calculate most right column and most bottom row
-        var endRow = startRow - this.vpRows;
-        var endCol = startCol + this.vpCols;
-
-        // Make sure the loop gets as big as possible
-        // This is so that we can remove old entries that is offscreen now
-        var si = Math.max(this.oldStartRow, startRow);
-        var sj = Math.min(this.oldStartCol, startCol);
-        var ei = Math.min(this.oldStartRow - Math.max(this.oldvpRows, this.vpRows), endRow);
-        var ej = Math.max(this.oldStartCol + Math.max(this.oldvpCols, this.vpCols), endCol);
-
-        // Loop, i = row, j = column
-        /*for(var i = si; i >= ei; i--) {
-            for(var j = sj; j <= ej; j++) {
-                if(i > startRow || i < endRow || j < startCol || j > endCol) {
-                    if(this.shownMap[i] && this.shownMap[i][j]) {
-                        $('#map_'+j+'_'+i).remove();
-                        this.shownMap[i][j] = null;
-                    }
-                } else {
-                    if(!this.shownMap[i]) this.shownMap[i] = [];
-                    if(!this.shownMap[i][j]) {
-                        var timg = $(document.createElement('img'));
-                        timg.attr({'id': 'map_'+j+'_'+i, 'src': 'images/map_'+j+'_'+i+'.png'});
-                        timg.css({'position': 'absolute', 
-                                  'width': this.tilesize+'px',
-                                  'height': this.tilesize+'px', 
-                                  'top': (i*this.tilesize*-1)+'px',
-                                  'left': (j*this.tilesize)+'px',
-                                  'MozUserSelect': 'none'});
-                        $(this.map).append(timg);
-                        this.shownMap[i][j] = true;
-                        timg = null;
+    MapCreator.prototype.draw = function() {
+        if(this.objects) {
+            universe = this.objects[0];
+            for(var i in universe.contains) {
+                galaxy = this.objects[universe.contains[i]];
+                for(var j in galaxy.contains) {
+                    obj = this.objects[galaxy.contains[j]];
+                    var x = (obj.pos[0] / universe.size) * 120000;
+                    var y = (obj.pos[1] / universe.size) * 120000;
+                    if(obj.type == 'Star System') {
+                        this.drawstarsystem(x, y, obj.id);
+                    } else if(obj.type == 'Fleet') {
+                        this.drawfleet(x, y, obj.id);
                     }
                 }
             }
-        }*/
-        this.oldStartRow = startRow;
-        this.oldStartCol = startCol;
-    }
+        }
+    };
 
-    this.drawstarsystem = function(x, y, id) {
+    MapCreator.prototype.drawstarsystem = function(x, y, id) {
         var test = $(document.createElement('div'));
         test.attr('id', id).attr('class', 'starsystem');
-        test.css({'position': 'absolute', 'top': (this.tilesize) - y+'px', 'left': x+'px', 'background-color': 'red', 
+        test.css({'position': 'absolute', 'top': -y+'px', 'left': x+'px', 'background-color': 'red', 
                 'width': '10px', 'height': '10px', 'font-size': '8px', 'z-index': '100'});
         this.canvas.append(test);
-    }
+    };
 
-    this.drawfleet = function(x, y, id) {
+    MapCreator.prototype.drawfleet = function(x, y, id) {
         var test = $(document.createElement('div'));
         test.attr('id', id).attr('class', 'fleet');
-        test.css({'position': 'absolute', 'top': (this.tilesize) - y+'px', 'left': x+'px', 'background-color': 'green', 
+        test.css({'position': 'absolute', 'top': -y+'px', 'left': x+'px', 'background-color': 'green', 
                 'width': '10px', 'height': '10px', 'font-size': '8px', 'z-index': '100'});
         this.canvas.append(test);
-    }       
-}
+    };
+
+    return new MapCreator();
+} )();
 
 Logger = ( function() {
     var LoggerClass = function(){};
@@ -423,57 +358,33 @@ UserInterface = ( function() {
      * Store all objects
      */
     var objects = null;
-    var map = null;
 
-    var getObjects = function() {
+    var constructor = function(){};
+
+    constructor.prototype.drawUI = function() {
+        UILock.create().notice('Please wait while loading user interface <img src="/images/loading.gif" />');
+        $('#loginbox').hide();
+        $('#ui').show();
+        this.getObjects();
+        Map.init("#mapdiv", 0, 0);
+        Map.draw();
+        UILock.clear();
+    };
+
+    constructor.prototype.getObjects = function() {
         $.ajax({async: false, type: "GET", dataType: 'json', url: "/json/get_objects/", 
             error: function(data, textstatus) { }, 
             success: function(data, textstatus) {
                 if(data.auth === true) {
+                    console.dir(this);
                     TurnHandler.setTime(data.time);
+                    Map.addObjects(data.objects);
                     objects = data.objects;
                 } else {
                     this.logout();
                 }
             }
         });
-    };
-
-    var drawMap = function() {
-        universe = objects[0];
-        for(var i in universe.contains) {
-            galaxy = objects[universe.contains[i]];
-            for(var j in galaxy.contains) {
-                obj = objects[galaxy.contains[j]];
-                var x = (obj.pos[0] / universe.size) * 120000;
-                var y = (obj.pos[1] / universe.size) * 120000;
-                if(obj.type == 'Star System') {
-                    map.drawstarsystem(x, y, obj.id);
-                } else if(obj.type == 'Fleet') {
-                    map.drawfleet(x, y, obj.id);
-                }
-            }
-        }
-    }
-
-    var drawUI = function() {
-        UILock.create().notice('Please wait while loading user interface <img src="/images/loading.gif" />');
-        EventHandler.notify('ui.draw');
-        UILock.clear();
-    };
-
-    var rest = function() {
-        $('#loginbox').hide();
-        $('#ui').show();
-
-        map = new DragTileMap('#mapdiv');
-        map.init(0, 0);
-    };
-
-    var constructor = function(){};
-
-    constructor.prototype.drawUI = function() {
-        drawUI();
     };
 
     constructor.prototype.isLoggedin = function() {
@@ -492,7 +403,7 @@ UserInterface = ( function() {
             success: function(data, textstatus) {
                 if(data.auth === true && data.cache === true) {
                     if(forceRedraw === true) {
-                        drawUI();
+                        this.drawUI();
                     } else {
                         TurnHandler.notice();   
                     }
@@ -537,13 +448,10 @@ UserInterface = ( function() {
 
     constructor.prototype.setup = function() {
 
+
         // Setup event subscribers
         EventHandler.subscribe("login.complete", [this, 'cache_update', true]);
         EventHandler.subscribe("login.complete", function() { UILock.notice('Please wait while the user interface is loading <img src="/images/loading.gif" />'); });
-
-        EventHandler.subscribe("ui.draw", rest);
-        EventHandler.subscribe("ui.draw", getObjects);
-        EventHandler.subscribe("ui.draw", drawMap);
 
         TurnHandler.register("#turn span.time", this.cache_update);
 
